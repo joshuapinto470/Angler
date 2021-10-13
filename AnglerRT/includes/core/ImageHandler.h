92fef7cdd7f8e1b01dd1cddeb0a790ec68329b43
Joshua Pinto <joshuapinto470@gmail.com>
2021-10-13 19:34:34 +0530

    Updated Image Handler class Updated image handler class to now include getWidth, getHeight  and getBufferCopy methods.

--- AnglerRT/includes/core/ImageHandler.h
+++ AnglerRT/includes/core/ImageHandler.h
@@ -2,11 +2,13 @@
 
 #include "Geometry.h"
 #include "Utilities.h"
-#include <iostream>
 #include <fstream>
+#include <iostream>
 
-class Image{  
-    public:
-        int width{}, height{};
-        virtual void Write(const Color&) = 0;
+class Image {
+  public:
+    virtual void Write(const Color &) = 0;
+    virtual std::unique_ptr<uint8_t[]> getBufferCopy() const = 0;
+    virtual unsigned getHeight() const = 0;
+    virtual unsigned getWidth() const = 0;
 };
